f45dccda11ca0b21667c436dd8c70533747a3f0c
Helwena <helwena8@gmail.com>
2025-01-03 19:53:52 +0100

    feat: create NavBar component and add icons

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import addIcon from "../assets/images/add.png";
+import bookmarkIcon from "../assets/images/bookmark.png";
+import homeIcon from "../assets/images/home-2.png";
+import searchIcon from "../assets/images/search.png";
+import imgProfile from "../assets/images/user-solid.svg";
+import "../style/NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <img src={homeIcon} alt="Home" className="nav-icon" />
+      <img src={bookmarkIcon} alt="Bookmark" className="nav-icon" />
+      <img src={addIcon} alt="Add" className="nav-icon" />
+      <img src={searchIcon} alt="Search" className="nav-icon" />
+      <img src={imgProfile} alt="Profile" className="nav-icon-profile-style" />
+      <div />
+    </nav>
+  );
+}
