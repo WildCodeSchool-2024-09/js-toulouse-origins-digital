208a36e029e14301be87a7793c80d929645817c1
Helwena <helwena8@gmail.com>
2025-01-08 10:10:14 +0100

    feat: add search bar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
 import HomeScreen from "./pages/HomeScreen";
 
 function App() {
   return (
     <>
       <HomeScreen />
-      <NavBar />
     </>
   );
 }
