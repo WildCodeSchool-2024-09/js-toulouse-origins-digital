083460a6a5c27906872a411cdf6f5698efac095a
TSMB54 <tom.schricke@gmail.com>
2025-01-09 14:28:41 +0100

    feat: schema sql

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,14 @@ function App() {
     <>
       <HomeScreen />
       <HomePage />
+
+      {/* <div><iframe src="https://app.videas.fr/embed/media/6eac5642-9cc6-4f89-80d8-cbef13ebeaed/"></iframe></div>
+     
+     <iframe src="https://app.videas.fr/embed/media/e965af55-6b93-4931-b17a-dab3f6a9424d/" className="iframe"></iframe>
+     <img src="https://cdn.videas.fr/v-medias/uploads/dc78edf1-5d1e-4087-a600-1bbe902d4d8a" alt="" />
+     <video controls width="300px">
+        <source src="https://cdn.videas.fr/v-medias/s5/hlsv1/61/f9/61f9cd40-cde2-4512-aa83-1d5900061cc6/playlist.m3u8" type="video/mp4" />
+     </video>  */}
     </>
   );
 }
