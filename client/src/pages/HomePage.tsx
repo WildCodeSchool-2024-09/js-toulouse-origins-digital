3e43ab83a58d904bafb062c8f9968f0c1e39a409
Helwena <helwena8@gmail.com>
2025-01-10 10:56:21 +0100

    feat: add User Sign Up component

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,12 +2,14 @@ import CarouselPrimary from "../components/CarouselPrimary";
 import CarouselVideo from "../components/CarouselVideo";
 import NavBar from "../components/NavBar";
 import UserLogin from "../components/UserLogin";
+import UserSignUp from "../components/UserSignUp";
 import "../styles/HomePage.css";
 
 export default function HomePage() {
   return (
     <>
       <UserLogin />
+      <UserSignUp />
       <CarouselPrimary />
       <h2 className="title-home-page">Catégories &#x27E9;</h2>
       <hr className="line" />
