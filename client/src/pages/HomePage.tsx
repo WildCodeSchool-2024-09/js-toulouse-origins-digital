15c993d6ae1bbffc269605210120ea662118fecd
Helwena <helwena8@gmail.com>
2025-01-15 12:25:45 +0100

    feat: add reset password component

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import CarouselPrimary from "../components/CarouselPrimary";
 import CarouselVideo from "../components/CarouselVideo";
 import ForgotPassword from "../components/ForgotPassword";
 import NavBar from "../components/NavBar";
+import ResetPassword from "../components/ResetPassword";
 import UserLogin from "../components/UserLogin";
 import UserSignUp from "../components/UserSignUp";
 import "../styles/HomePage.css";
@@ -12,6 +13,7 @@ export default function HomePage() {
       <UserLogin />
       <UserSignUp />
       <ForgotPassword />
+      <ResetPassword />
       <CarouselPrimary />
       <h2 className="title-home-page">Catégories &#x27E9;</h2>
       <hr className="line" />
