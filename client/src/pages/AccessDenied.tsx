b2585901732cfb931527497e6a1cf361560bb6a0
sylviabarraud2@gmail.com <sylviabarraud2@gmail.com>
2025-02-05 20:46:37 +0100

    feat: access denied page for playlists and favorites

--- client/src/pages/AccessDenied.tsx
+++ client/src/pages/AccessDenied.tsx
@@ -4,9 +4,9 @@ export default function AccessDenied() {
   return (
     <div>
       <Header />
-      <div className="container access denied">
-        <section className="section access denied">
-          <p>
+      <div className="container-access-denied">
+        <section className="section-access-denied">
+          <p className="paragraph-access-denied">
             Il semblerait que tu essaies d'accéder à une page sans droit d'accès
             ! Pour retourner à la page d'accueil de Jestone clique sur{" "}
             <a href="http://localhost:3000/">Accueil Jestone</a>
