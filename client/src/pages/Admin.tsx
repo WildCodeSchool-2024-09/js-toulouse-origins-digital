e7d0e9bd467c5d99d20b9b1258eb93a423b47761
sylviabarraud2@gmail.com <sylviabarraud2@gmail.com>
2025-02-06 10:19:47 +0100

    fix: admin page access

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -4,6 +4,7 @@ import CardUserManager from "../components/CardUserManager";
 import CardVideoManager from "../components/CardVideoManager";
 import Header from "../components/Header";
 import "../styles/Admin.css";
+import { useOutletContext } from "react-router-dom";
 import categoriesPic from "../assets/images/category-management.png";
 import usersPic from "../assets/images/users-management.png";
 import videosPic from "../assets/images/video-management.png";
@@ -12,6 +13,7 @@ import ModalUserManager from "../components/ModalUserManager";
 import ModalVideoManager from "../components/ModalVideoManager";
 import NavBar from "../components/NavBar";
 import useModal from "../services/useModal";
+import AccessDenied from "./AccessDenied";
 
 const truncateText = (text: string, maxLength = 80) => {
   if (text.length <= maxLength) return text;
@@ -60,6 +62,11 @@ interface User {
   avatar_url: string;
   is_admin: boolean;
 }
+
+type Auth = {
+  user: User;
+  token: string;
+};
 
 export default function Admin() {
   const [category, setCategory] = useState<Category[]>([]);
@@ -117,7 +124,9 @@ export default function Admin() {
         const videosWithCategories = await Promise.all(
           videos.map(async (video: Video) => {
             const categoriesResponse = await fetch(
-              `${import.meta.env.VITE_API_URL}/api/videocategory/categories/${video.id}`,
+              `${import.meta.env.VITE_API_URL}/api/videocategory/categories/${
+                video.id
+              }`,
             );
             const videoCategories = await categoriesResponse.json();
             return {
@@ -139,214 +148,237 @@ export default function Admin() {
 
     fetchData();
   }, []);
+  const { auth } = useOutletContext() as { auth: Auth | null };
 
   return (
     <>
-      <ModalCategoryManager
-        isShowing={isShowingCategory}
-        hide={() => toggleCategory()}
-        category={editingCategory}
-        onSubmit={async (categoryData) => {
-          if (!editingCategory?.id) return;
+      {auth ? (
+        <>
+          <ModalCategoryManager
+            isShowing={isShowingCategory}
+            hide={() => toggleCategory()}
+            category={editingCategory}
+            onSubmit={async (categoryData) => {
+              if (!editingCategory?.id) return;
 
-          try {
-            const response = await fetch(
-              `${import.meta.env.VITE_API_URL}/api/categories/${editingCategory.id}`,
-              {
-                method: "PUT",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(categoryData),
-              },
-            );
+              try {
+                const response = await fetch(
+                  `${import.meta.env.VITE_API_URL}/api/categories/${
+                    editingCategory.id
+                  }`,
+                  {
+                    method: "PUT",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(categoryData),
+                  },
+                );
 
-            if (response.ok) {
-              setCategory((prevCategories) =>
-                prevCategories.map((c) =>
-                  c.id === editingCategory.id ? { ...c, ...categoryData } : c,
-                ),
-              );
-              toggleCategory();
-            } else {
-              console.error("Erreur lors de la mise à jour");
-            }
-          } catch (error) {
-            console.error("Erreur:", error);
-          }
-        }}
-      />
-      <ModalUserManager
-        isShowing={isShowingUser}
-        hide={() => toggleUser()}
-        user={editingUser}
-        onSubmit={async (userData) => {
-          try {
-            const response = await fetch(
-              `${import.meta.env.VITE_API_URL}/api/users/${editingUser?.id}`,
-              {
-                method: "PUT",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  email: userData.email,
-                  pseudo: userData.pseudo,
-                  is_admin: userData.is_admin,
-                  avatar_url: userData.avatar_url,
-                }),
-              },
-            );
-            if (response.ok) {
-              setUser((users) =>
-                users.map((u) =>
-                  u.id === editingUser?.id ? { ...u, ...userData } : u,
-                ),
-              );
-              toggleUser();
-            } else {
-              console.error("Erreur lors de la mise à jour");
-            }
-          } catch (error) {
-            console.error("Erreur:", error);
-          }
-        }}
-      />
-      <ModalVideoManager
-        isShowing={isShowingVideo}
-        hide={() => toggleVideo()}
-        video={editingVideo}
-        onSubmit={async (videoData) => {
-          try {
-            const videoResponse = await fetch(
-              `${import.meta.env.VITE_API_URL}/api/videos/${editingVideo?.id}`,
-              {
-                method: "PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(videoData),
-              },
-            );
-            const categoriesResponse = await fetch(
-              `${import.meta.env.VITE_API_URL}/api/videocategory/${editingVideo?.id}`,
-              {
-                method: "PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ categoryIds: videoData.categories }),
-              },
-            );
+                if (response.ok) {
+                  setCategory((prevCategories) =>
+                    prevCategories.map((c) =>
+                      c.id === editingCategory.id
+                        ? { ...c, ...categoryData }
+                        : c,
+                    ),
+                  );
+                  toggleCategory();
+                } else {
+                  console.error("Erreur lors de la mise à jour");
+                }
+              } catch (error) {
+                console.error("Erreur:", error);
+              }
+            }}
+          />
+          <ModalUserManager
+            isShowing={isShowingUser}
+            hide={() => toggleUser()}
+            user={editingUser}
+            onSubmit={async (userData) => {
+              try {
+                const response = await fetch(
+                  `${import.meta.env.VITE_API_URL}/api/users/${
+                    editingUser?.id
+                  }`,
+                  {
+                    method: "PUT",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      email: userData.email,
+                      pseudo: userData.pseudo,
+                      is_admin: userData.is_admin,
+                      avatar_url: userData.avatar_url,
+                    }),
+                  },
+                );
+                if (response.ok) {
+                  setUser((users) =>
+                    users.map((u) =>
+                      u.id === editingUser?.id ? { ...u, ...userData } : u,
+                    ),
+                  );
+                  toggleUser();
+                } else {
+                  console.error("Erreur lors de la mise à jour");
+                }
+              } catch (error) {
+                console.error("Erreur:", error);
+              }
+            }}
+          />
+          <ModalVideoManager
+            isShowing={isShowingVideo}
+            hide={() => toggleVideo()}
+            video={editingVideo}
+            onSubmit={async (videoData) => {
+              try {
+                const videoResponse = await fetch(
+                  `${import.meta.env.VITE_API_URL}/api/videos/${
+                    editingVideo?.id
+                  }`,
+                  {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(videoData),
+                  },
+                );
+                const categoriesResponse = await fetch(
+                  `${import.meta.env.VITE_API_URL}/api/videocategory/${
+                    editingVideo?.id
+                  }`,
+                  {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ categoryIds: videoData.categories }),
+                  },
+                );
 
-            if (videoResponse.ok && categoriesResponse.ok) {
-              setVideo((videos) =>
-                videos.map((vid) => {
-                  if (vid.id === editingVideo?.id) {
-                    return {
-                      ...vid,
-                      ...videoData,
-                      categories: videoData.categories, // Inclure les catégories mises à jour
-                    };
-                  }
-                  return vid;
-                }),
-              );
-              toggleVideo();
-            }
-          } catch (error) {
-            console.error("Erreur:", error);
-          }
-        }}
-      />
-      <Header />
-      <div className="admin-page">
-        <nav className="nav-admin">
-          <button
-            className={`button-nav-admin ${adminSection === "users" ? "button-nav-admin-active" : ""}`}
-            type="button"
-            onClick={() => setAdminSection("users")}
-          >
-            <img width={30} src={usersPic} alt="" />
-            <p>Users</p>
-          </button>
-          <button
-            className={`button-nav-admin ${adminSection === "categories" ? "button-nav-admin-active" : ""}`}
-            type="button"
-            onClick={() => setAdminSection("categories")}
-          >
-            <img width={30} src={categoriesPic} alt="" />
-            <p>Categories</p>
-          </button>
-          <button
-            className={`button-nav-admin ${adminSection === "videos" ? "button-nav-admin-active" : ""}`}
-            type="button"
-            onClick={() => setAdminSection("videos")}
-          >
-            <img width={30} src={videosPic} alt="" />
-            <p>Videos</p>
-          </button>
-        </nav>
-        <h2 className="admin-title">
-          Gestion des{" "}
-          {adminSection === "categories"
-            ? "catégories"
-            : adminSection === "videos"
-              ? "vidéos"
-              : "utilisateurs"}
-        </h2>
+                if (videoResponse.ok && categoriesResponse.ok) {
+                  setVideo((videos) =>
+                    videos.map((vid) => {
+                      if (vid.id === editingVideo?.id) {
+                        return {
+                          ...vid,
+                          ...videoData,
+                          categories: videoData.categories, // Inclure les catégories mises à jour
+                        };
+                      }
+                      return vid;
+                    }),
+                  );
+                  toggleVideo();
+                }
+              } catch (error) {
+                console.error("Erreur:", error);
+              }
+            }}
+          />
+          <Header />
+          <div className="admin-page">
+            <nav className="nav-admin">
+              <button
+                className={`button-nav-admin ${
+                  adminSection === "users" ? "button-nav-admin-active" : ""
+                }`}
+                type="button"
+                onClick={() => setAdminSection("users")}
+              >
+                <img width={30} src={usersPic} alt="" />
+                <p>Users</p>
+              </button>
+              <button
+                className={`button-nav-admin ${
+                  adminSection === "categories" ? "button-nav-admin-active" : ""
+                }`}
+                type="button"
+                onClick={() => setAdminSection("categories")}
+              >
+                <img width={30} src={categoriesPic} alt="" />
+                <p>Categories</p>
+              </button>
+              <button
+                className={`button-nav-admin ${
+                  adminSection === "videos" ? "button-nav-admin-active" : ""
+                }`}
+                type="button"
+                onClick={() => setAdminSection("videos")}
+              >
+                <img width={30} src={videosPic} alt="" />
+                <p>Videos</p>
+              </button>
+            </nav>
+            <h2 className="admin-title">
+              Gestion des{" "}
+              {adminSection === "categories"
+                ? "catégories"
+                : adminSection === "videos"
+                  ? "vidéos"
+                  : "utilisateurs"}
+            </h2>
 
-        {adminSection !== "users" && (
-          <button className="add-card-manager" type="button">
-            + Ajouter
-          </button>
-        )}
-        {isLoading ? (
-          <div>Chargement en cours...</div>
-        ) : (
-          <>
-            {adminSection === "categories" &&
-              category.map((cat) => (
-                <CardCategoryManager
-                  key={cat.id}
-                  id={cat.id}
-                  name={cat.name}
-                  description={cat.description}
-                  url_image={cat.url_image}
-                  onEdit={() => toggleCategory(cat.id)}
-                  onDelete={handleCategoryDelete}
-                />
-              ))}
-            {adminSection === "videos" &&
-              video.map((vid) => (
-                <CardVideoManager
-                  id={vid.id}
-                  key={vid.id}
-                  title={vid.title}
-                  description={truncateText(vid.description)}
-                  duration={formatTime(vid.date.toString())}
-                  video_url={vid.video_url}
-                  date={formatDate(vid.date.toString())}
-                  views={vid.views}
-                  categories={vid.categories}
-                  onEdit={() => toggleVideo(vid.id)}
-                  onDelete={handleVideoDelete}
-                />
-              ))}
-            {adminSection === "users" &&
-              user.map((user) => (
-                <CardUserManager
-                  id={user.id}
-                  key={user.id}
-                  pseudo={user.pseudo}
-                  email={user.email}
-                  avatar_url={user.avatar_url}
-                  is_admin={user.is_admin}
-                  onEdit={() => toggleUser(user.id)}
-                  onDelete={handleUserDelete}
-                />
-              ))}
-          </>
-        )}
-      </div>
-      <div className="fill-the-gap" />
-      <NavBar />
+            {adminSection !== "users" && (
+              <button className="add-card-manager" type="button">
+                + Ajouter
+              </button>
+            )}
+            {isLoading ? (
+              <div>Chargement en cours...</div>
+            ) : (
+              <>
+                {adminSection === "categories" &&
+                  category.map((cat) => (
+                    <CardCategoryManager
+                      key={cat.id}
+                      id={cat.id}
+                      name={cat.name}
+                      description={cat.description}
+                      url_image={cat.url_image}
+                      onEdit={() => toggleCategory(cat.id)}
+                      onDelete={handleCategoryDelete}
+                    />
+                  ))}
+                {adminSection === "videos" &&
+                  video.map((vid) => (
+                    <CardVideoManager
+                      id={vid.id}
+                      key={vid.id}
+                      title={vid.title}
+                      description={truncateText(vid.description)}
+                      duration={formatTime(vid.date.toString())}
+                      video_url={vid.video_url}
+                      date={formatDate(vid.date.toString())}
+                      views={vid.views}
+                      categories={vid.categories}
+                      onEdit={() => toggleVideo(vid.id)}
+                      onDelete={handleVideoDelete}
+                    />
+                  ))}
+                {adminSection === "users" &&
+                  user.map((user) => (
+                    <CardUserManager
+                      id={user.id}
+                      key={user.id}
+                      pseudo={user.pseudo}
+                      email={user.email}
+                      avatar_url={user.avatar_url}
+                      is_admin={user.is_admin}
+                      onEdit={() => toggleUser(user.id)}
+                      onDelete={handleUserDelete}
+                    />
+                  ))}
+              </>
+            )}
+          </div>
+          <div className="fill-the-gap" />
+          <NavBar />
+        </>
+      ) : (
+        <AccessDenied />
+      )}
     </>
   );
 }
